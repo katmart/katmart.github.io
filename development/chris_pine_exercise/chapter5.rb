10d468ca25849febf14a7c714a3608f3c3ae32fd
Kat Martineau <martineaukt@gmail.com>
2015-06-21 23:12:01 -0400

    Coursework

---
+++ development/chris_pine_exercise/chapter5.rb
@@ -0,0 +1,24 @@
+#Exercise 5.5 - Chomp Method The chomp method ensures that the puts line immediately after it
+#doesn't end up going on two separate lines, in this case.
+
+#puts "Hello there, and what\'s your name?"
+#name = gets.chomp
+#puts "Your name is " + name + "? What a lovely name!"
+#puts "Pleased to meet you, " + name + ". :)"
+
+#methods case
+#letters = 'aAbBcCddeE'
+#puts letters.upcase
+#puts letters.downcase
+#puts letters.swapcase
+#puts letters.capitalize
+#puts ' a'.capitalize
+#puts letters
+
+line_width = 50
+puts (        "Kit Kat Attack".center(line_width))
+puts (         "Ate her tic tac".center(line_width))
+puts (         "It was spearment, and nor orange".center(line_width))
+puts (          "When along came a pitbull".center(line_width))
+puts (           "Who sat on the begging carpet".center(line_width))
+puts (          "Sad that tic tacs have no meatz".center(line_width))
---
+++ development/variables.rb
@@ -0,0 +1,36 @@
+
+#MULTIPLYING A STRING
+#Susie = "I saw Susie sitting in a shoe shine shop. Where she sits she shines, and where she shines she sits. \n"
+#x = 20
+#puts Susie.length
+#changeable variable
+
+#x = "748583".to_i
+#y = "3".to_i
+
+#number = x*y
+#puts number
+
+#x = "KatherineMartineau"
+
+#puts x.upcase
+#puts x.reverse
+
+
+#PUTS Exercise 2.2
+puts "What is your name?"
+First_Name = gets.chomp
+puts "Hallo, " + First_Name.capitalize + "! I want to know your middle name! Can you tell me?"
+Middle_Name = gets.chomp
+puts "Hallo, " + First_Name.capitalize + " " + Middle_Name.capitalize + "! I bet no one ever calls you that! I want to know your full name. What is your last name?"
+Last_Name = gets
+puts 'Now I know your full name, ' + First_Name.upcase + " "+ Middle_Name.upcase + " " + Last_Name.upcase
+
+
+#Exercise 2.2
+
+#puts "What is your favorite number"
+#Favorite_Number = gets.to_i
+#Favorite_Number_1 = Favorite_Number + 1
+#puts "Do you want a higher number? C'mon! I believe your favorite number is actually " + Favorite_Number_1.to_s
+
